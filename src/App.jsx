026408df7fb9c7aac6a29f41205feaa50bd130db
RisheeShukla <risheeshukla0@gmail.com>
2026-01-26 23:00:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import {motion,useScroll} from "framer-motion"
+import './index.css';
+
+const App=()=>{
+     const{scrollYProgress:completionProgress}=useScroll()
+     const svgIconVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(252, 211, 77, 0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(252, 211, 77, 1)",
+  },
+}
+
+  return (
+
+   <div className='flex flex-col gap-10 overflow-x-hidden'>
+   <motion.section variants={{hidden:{opacity:0},show:{opacity:1,transition:{staggerChildren:0.25},},}} initial="hidden" animate="show" className="grid grid-cols-3 p-10 gap-10">
+    <motion.div variants={{hidden:{opacity:0},show:{opacity:1},}} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+      <motion.div className="w-20 h-20 gap-5 bg-stone-100 rounded-lg"
+       initial={{opacity:0,y:100}}
+       animate={{opacity:1,y:0}}
+       transition={{duration:1,ease:"easeOut",repeat:Infinity,delay:0.2}}
+
+      
+      />
+      <motion.div className="w-20 h-20 gap-5 bg-stone-100 rounded-full"
+      initial={{opacity:0,y:-100}}
+       animate={{opacity:1,y:0}}
+       transition={{duration:1,ease:"easeOut",delay:0.4,repeat:Infinity}} />
+    </motion.div>
+    <motion.div variants={{hidden:{opacity:0},show:{opacity:1},}} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+    <motion.div  className="w-40 h-40 shadow-md bg-rose-400"
+    animate={{scale:[1,2,2,1],rotate:[0,90,90,0],borderRadius:['10%','50%','50%','10%']}}
+    transition={{duration:5,ease:'easeOut',repeat:Infinity,repeatDelay:1}}
+    />
+    </motion.div>
+    <motion.div variants={{hidden:{opacity:0},show:{opacity:1},}} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+      <motion.button className="bg-emerald-600  w-1/2 py-4 rounded-lg text-2xl text-grey-400 font-light tracking-wide"
+      whileTap={{scale:0.9}}
+      whileHover={{scale:1.1,backgroundColor:'#1f70eaff',color:'black'}}
+      > 
+        SUBSCRIBE
+      </motion.button>
+    </motion.div>
+    <motion.div variants={{hidden:{opacity:0},show:{opacity:1},}} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+      <motion.div className="w-20 h-20 rounded-lg bg-orange-400  cursor-grab"
+      drag
+      dragConstraints={{top:-125,
+        right:125,
+        bottom:125,
+        left:-125
+
+      }}
+      dragTransition={{bounceStiffness:400,bounceDamping:10}}
+      />
+    </motion.div>
+    <motion.div variants={{hidden:{opacity:0},show:{opacity:1},}} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+      <motion.div className="w-40  aspect-square bg-gray-50/20 rounded-xl ">
+      <motion.div className="w-full  bg-gray-400 rounded-xl h-full origin-bottom" 
+      style={{scaleY:completionProgress}}
+      
+      />
+      </motion.div>
+    </motion.div>
+    <motion.div variants={{hidden:{opacity:0},show:{opacity:1},}} className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+    <motion.svg xmlns='http://www.w3.org/2000/svg' viewBox="0 0 24 24" className='w-1/2 stroke-amber-500 stroke=[0.5]'>
+     <motion.path
+              d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+              variants={svgIconVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{
+                default: {
+                  duration: 3,
+                  ease: "easeInOut",
+                  delay: 1,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1,
+                },
+                fill: {
+                  duration: 3,
+                  ease: "easeIn",
+                  delay: 2,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1,
+                },
+              }}
+            />
+    </motion.svg>
+    </motion.div>
+     </motion.section>
+    </div>
+  )
+}
+
+export default App
